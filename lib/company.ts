1cec60a73144ed80494f4beb1247ac31947b7030
shawon21-lab <j88394772@gmail.com>
2025-08-16 13:08:37 +0100

    Update company.ts

--- lib/company.ts
+++ lib/company.ts
@@ -1,8 +1,40 @@
+// lib/company.ts
 import mongooseConnect from "@/lib/mongoose";
 import Company from "@/models/Company";
 
-export const getCompanyData = async () => {
+export interface CompanyProps {
+  _id?: string;
+  name: string;
+  head?: {
+    title?: string;
+    description?: string;
+    iconUrl?: string;
+  };
+}
+
+export const getCompanyData = async (): Promise<CompanyProps> => {
   await mongooseConnect();
-  const companies = await Company.find({});
-  return companies[0] || null;
+
+  let initialCompany: CompanyProps | null;
+  const companies = await Company.find<CompanyProps>({});
+
+  if (companies.length <= 0) {
+    // If no company exists, create one with defaults
+    const newCompany = new Company({
+      name: "Enter company name",
+      head: {
+        title: "Default Title",
+        description: "Default Description",
+        iconUrl: "https://example.com/image.jpg",
+      },
+    });
+    const savedCompany = await newCompany.save();
+    initialCompany = savedCompany.toObject();
+  } else {
+    initialCompany = companies[0].toObject();
+  }
+
+  return JSON.parse(JSON.stringify(initialCompany));
 };
+
+
